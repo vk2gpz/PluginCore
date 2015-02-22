a602b9d6ca4c7f80a6f7d445b2d2922237c24b44
sycholic <sycholic@spigotmc.org>
2015-02-22 17:37:08 -0500

    Formatting Cleanup

--- src/main/java/org/yi/acru/bukkit/PluginCoreServerListener.java
+++ src/main/java/org/yi/acru/bukkit/PluginCoreServerListener.java
@@ -3,8 +3,6 @@
 // Distributed under the The Non-Profit Open Software License version 3.0 (NPOSL-3.0)
 // http://www.opensource.org/licenses/NOSL3.0
 //
-
-
 package org.yi.acru.bukkit;
 
 //Imports.
@@ -20,48 +18,36 @@ import org.bukkit.event.server.PluginDisableEvent;
 
 import org.bukkit.plugin.PluginManager;
 
-
 // extends ServerListener
-public class PluginCoreServerListener implements Listener{
-	private static PluginCore		plugin;
-	
-	
-	public PluginCoreServerListener(PluginCore instance){
-		plugin = instance;
-	}
-	
-	
-	protected void registerEvents(){
-		PluginManager	pm = plugin.getServer().getPluginManager();
+public class PluginCoreServerListener implements Listener {
 
-		pm.registerEvents(this, plugin);
-	}
-	
-	
-	//********************************************************************************************************************
-	// Start of event section
-	
-	
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void onPluginEnable(PluginEnableEvent event){
-		plugin.setLink(event.getPlugin().getDescription().getName(), true, true);
-	}
-	
-	
-	@EventHandler(priority = EventPriority.NORMAL)
-	public void onPluginDisable(PluginDisableEvent event){
-		plugin.setLink(event.getPlugin().getDescription().getName(), false, true);
-	}
-	
-	
+    private static PluginCore plugin;
+
+    public PluginCoreServerListener(PluginCore instance) {
+        plugin = instance;
+    }
+
+    protected void registerEvents() {
+        PluginManager pm = plugin.getServer().getPluginManager();
+
+        pm.registerEvents(this, plugin);
+    }
+
+    //********************************************************************************************************************
+    // Start of event section
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPluginEnable(PluginEnableEvent event) {
+        plugin.setLink(event.getPlugin().getDescription().getName(), true, true);
+    }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onPluginDisable(PluginDisableEvent event) {
+        plugin.setLink(event.getPlugin().getDescription().getName(), false, true);
+    }
+
 	//public void onServerCommand(ServerCommandEvent event){}
-	//public void onMapInitialize(MapInitializeEvent event){}
-	//public void onServerListPing(ServerListPingEvent event){}
-	
-	
-	//********************************************************************************************************************
-	// Start of utility section
-	
-	
+    //public void onMapInitialize(MapInitializeEvent event){}
+    //public void onServerListPing(ServerListPingEvent event){}
+    //********************************************************************************************************************
+    // Start of utility section
 }
-
